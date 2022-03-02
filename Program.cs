8a3d51caf0f16203b9c3d2332af85e1c929f1c70
RainbowFrogi <s2000956@edu.bc.fi>
2022-03-02 14:39:38 +0200

    Add --personcount and --interactive command line switches

--- Program.cs
+++ Program.cs
@@ -6,26 +6,44 @@ namespace Faker
     {
         static void Main(string[] args)
         {
-            int i = 1;
+            Console.WriteLine("Type --personcount, or --interactive");
+            string count = Console.ReadLine();
 
-            while (true)
+            if (count != null)
             {
-
-                var name = Faker.Name.FullName();
-                var address = Faker.Address.StreetName();
-                var driversLicense = Faker.Identification.SocialSecurityNumber();
-                var passportNum = Faker.Identification.UkPassportNumber();
-                var mBI = Faker.Identification.MedicareBeneficiaryIdentifier();
-
-                Console.WriteLine($"Name: {name}, Address: {address}, Drivers license: {driversLicense}, Passport number {passportNum} and Medicare beneficiary identifier: {mBI}");
-                Console.WriteLine();
-
-                if(i == 3)
+                if(count == "--personcount")
                 {
-                    break;
+                    NewMethod();
+                    NewMethod();
+                    NewMethod();
                 }
-                i++;
-            }    
+                else if(count == "--interactive")
+                {
+                    Console.WriteLine("How many fake persons data you want?");
+                    int amount = Convert.ToInt32(Console.ReadLine());
+
+                    int i = 0;
+                    while(i < amount)
+                    {
+                        NewMethod();
+
+                        i++;
+                    }
+                }
+            }
+        }
+
+        private static void NewMethod()
+        {
+            Console.WriteLine("");
+
+            var name = Faker.Name.FullName();
+            var address = Faker.Address.StreetName();
+            var driversLicense = Faker.Identification.SocialSecurityNumber();
+            var passportNum = Faker.Identification.UkPassportNumber();
+            var mBI = Faker.Identification.MedicareBeneficiaryIdentifier();
+
+            Console.WriteLine($"Name: {name}, Address: {address}, Drivers license: {driversLicense}, Passport number {passportNum} and Medicare beneficiary identifier: {mBI}");
         }
     }
 }
